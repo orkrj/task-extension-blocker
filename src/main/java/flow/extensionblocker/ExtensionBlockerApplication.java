68076af3912af497768c0cef7c106ae319912b4a
zun <k5orkrj@gmail.com>
2025-07-16 22:37:25 +0900

    feat: initialize project settings

---
+++ src/main/java/flow/extensionblocker/ExtensionBlockerApplication.java
@@ -0,0 +1,13 @@
+package flow.extensionblocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionBlockerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExtensionBlockerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/flow/extensionblocker/ExtensionBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.extensionblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionBlockerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
